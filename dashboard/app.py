46dbb9c5e75612cd9d9407c69409f0df3af3a2e9
notangy <nnotarangelo7@gmail.com>
2025-08-24 20:04:44 +0100

    show username if found in .env

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+load_dotenv()
+
+username = os.environ.get("AO3_USERNAME")
 
 
 @app.route("/")
 def index():
     # TODO return stats here
-    return render_template("index.html", stats=None)
+    return render_template("index.html", username=username, stats=None)
 
 
 if __name__ == "__main__":
